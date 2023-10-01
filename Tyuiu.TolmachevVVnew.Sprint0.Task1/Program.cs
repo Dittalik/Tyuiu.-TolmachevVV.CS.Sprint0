e2acc24552601dc6296a0817dbe9a431c49b0056
Vitaly Tolmachev <vitalhare@outlook.com>
2023-10-01 19:56:46 +0500

    Task1

---
+++ Tyuiu.TolmachevVVnew.Sprint0.Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.TolmachevVVnew.Sprint0.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello wolrldsle");
+            string name = "Виталий";
+            int age = 21;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет");
+            Console.ReadKey();
+        }
+    }
+}
