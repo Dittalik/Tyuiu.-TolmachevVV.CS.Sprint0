cc63c37277504d9adf94b74473aa553a8cbafd8d
Vitaly Tolmachev <vitalhare@outlook.com>
2023-10-01 20:43:53 +0500

    Task3

---
+++ Tyuiu.TolmachevVVnew.Sprint0.Task3.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TolmachevVVnew.Sprint0.Task3.Lib;
+
+namespace Tyuiu.TolmachevVVnew.Sprint0.Task3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckSumValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.TolmachevVVnew.Sprint0.Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TolmachevVVnew.Sprint0.Task3.Lib;
+
+namespace Tyuiu.TolmachevVVnew.Sprint0.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
